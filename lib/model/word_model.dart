2445819c56841dc07127099c9634c04deb4a6148
Harsh <deadspaceharsh@gmail.com>
2021-04-05 12:06:51 +0530

    fixed model class

--- lib/model/word_model.dart
+++ lib/model/word_model.dart
@@ -1,30 +1,51 @@
 
 class WordDefinition {
-  WordDefinition(
-      {this.wordtype,
-      this.worddefinition,
-      this.wordexample,
-      this.wordimage_url,
-      this.wordemoji,
-      this.word,
-      this.wordpronunciation});
-  String wordtype;
-  String worddefinition;
-  String wordexample;
-  String wordimage_url;
-  String wordemoji;
-  String word;
-  String wordpronunciation;
+  WordDefinition({
+    this.definitions,
+    this.word,
+    this.pronunciation,
+  });
 
-  factory WordDefinition.fromJson(Map<String, dynamic> parsedJson) {
-    return WordDefinition(
-      wordtype: parsedJson['definitions']['type'],
-      worddefinition: parsedJson['definitions']['definition'],
-      wordexample: parsedJson['definitions']['example'],
-      wordimage_url: parsedJson['definitions']['image_url'],
-      wordemoji: parsedJson['definitions']['emoji'],
-      word: parsedJson['word'],
-      wordpronunciation: parsedJson['pronunciation'],
-    );
-  }
+  List<Definition> definitions;
+  String word;
+  String pronunciation;
+
+  factory WordDefinition.fromJson(Map<String, dynamic> json) => WordDefinition(
+    definitions: List<Definition>.from(json["definitions"].map((x) => Definition.fromJson(x))),
+    word: json["word"],
+    pronunciation: json["pronunciation"],
+  );
+
+}
+
+class Definition {
+  Definition({
+    this.type,
+    this.definition,
+    this.example,
+    this.imageUrl,
+    this.emoji,
+  });
+
+  String type;
+  String definition;
+  String example;
+  dynamic imageUrl;
+  String emoji;
+
+  factory Definition.fromJson(Map<String, dynamic> json) => Definition(
+    type: json["type"],
+    definition: json["definition"],
+    example: json["example"],
+    imageUrl: json["image_url"],
+    emoji: json["emoji"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "type": type,
+    "definition": definition,
+    "example": example,
+    "image_url": imageUrl,
+    "emoji": emoji,
+  };
 }
