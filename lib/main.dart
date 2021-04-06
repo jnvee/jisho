1a4dda83a38bef2f376ce3104bedacc11da67959
Harsh <deadspaceharsh@gmail.com>
2021-04-06 12:11:40 +0530

    hive model

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:jisho/ui/app_entry.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:jisho/model/bookmark_words.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  //TODO: initialize Hive
+  final documentDirectory = await getApplicationDocumentsDirectory();
+  Hive.init(documentDirectory.path);
+  Hive.registerAdapter(BookmarkWordsAdapter());
   runApp(AppEntry());
 }
